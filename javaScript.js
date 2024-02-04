e3848c5b671f23037b91c65a833d56692830076d
Prashant Yadav <154693743+Prashantyadav91@users.noreply.github.com>
2024-02-05 01:37:05 +0530

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,8 @@
+const email = document.querySelector(".Email");
+const email1 = document.querySelector(".Email1");
+email.addEventListener("click",() => {
+    email.value = "  ";
+});
+email1.addEventListener("click",() => {
+    email1.value = "  ";
+});
